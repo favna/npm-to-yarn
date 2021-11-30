627a69247452c10d811b1ed51dbe6bb5baa5ce61
Armano <armano2@users.noreply.github.com>
2021-11-30 17:22:46 +0100

    fix: add improved api (#3)

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,26 @@ import { yarnToNPM } from './yarnToNpm'
 import { npmToYarn } from './npmToYarn'
 
 /**
+ * Converts yarn to npm command
+ */
+export function convertToNpm(str: string) {
+  return str.replace(/yarn(?: +([^&\n\r]*))?/gm, yarnToNPM)
+}
+
+/**
+ * Converts npm to yarn command
+ */
+export function convertToYarn(str: string) {
+  return str.replace(/npm(?: +([^&\n\r]*))?/gm, npmToYarn)
+}
+
+/**
  * Converts between npm and yarn command
  */
 export default function convert(str: string, to: 'npm' | 'yarn'): string {
-  const returnStr = str
   if (to === 'npm') {
-    return returnStr.replace(/yarn(?: +([^&\n\r]*))?/gm, yarnToNPM)
+    return convertToNpm(str)
   } else {
-    return returnStr.replace(/npm(?: +([^&\n\r]*))?/gm, npmToYarn)
+    return convertToYarn(str)
   }
 }
